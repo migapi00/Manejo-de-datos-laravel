4ab5ec0c6aa781786318b4e8f9e46789298212a1
Luis <migapi00@gmail.com>
2023-03-13 13:03:01 +0100

    Manejo de datos

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function getAllProjects() {
+        $projects = Project::all();
+        return $projects;
+    }
+
+    // public function getAllProjects(){
+    //     $projects = Project::take(5)->get();
+    //     return $projects;
+    // }
+
+    public function insertProject() {
+        $project = new Project;
+        $project->city_id = 10;
+        $project->company_id = 10;
+        $project->user_id = 10;
+        $project->name = 'Proyecto Prueba';
+        $project->execution_date = '2020-06-16';
+        $project->is_active = 1;
+        $project->save();
+
+        return "Guardado";
+    }
+
+    public function crearRegistro() {
+
+            for ($i=1; $i < 30; $i++) {
+                $project = new Project;
+                $project->city_id = rand(1,12);
+                $project->company_id = rand(1,12);
+                $project->user_id = rand(1,12);
+                $project->name = "Proyecto {$i}";
+                $project->execution_date = "2020-04-{$i}";
+                $project->is_active = rand(0,1);
+                $project->save();
+            }
+                return "Registros exitosamente guardados ";
+        }
+
+        public function updateProject() {
+            $project = Project::find(40);
+            $project->name = 'Proyecto de tecnología';
+            $project->save();
+
+            return "Actualizado";
+        }
+
+        public function executionProject() {
+            Project::where('is_active', 1)
+                ->where('city_id', 4)
+                ->update(['execution_date' => '2020-02-03']);
+                return "Fecha de Ejecucion actualizada";
+        }
+
+        public function nameInactivosProject() {
+            Project::where('is_active', 0)
+                ->update(['name' => 'Proyecto Piloto']);
+                return "Trabajo Hecho";
+        }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProjectController;
+use App\Models\Project;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+
+});
+
+Route::get('getAllProjects', [ProjectController::class,'getAllProjects']);
+
+Route::get('insertProject', [ProjectController::class,'insertProject']);
+
+Route::get('crearRegistro', [ProjectController::class,'crearRegistro']);
+
+Route::get('updateProject', [ProjectController::class,'updateProject']);
+
+Route::get('executionProject', [ProjectController::class,'executionProject']);
+
+Route::get('nameInactivosProject', [ProjectController::class,'nameInactivosProject']);
